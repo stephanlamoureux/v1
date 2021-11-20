336c04eaf7581be0f596081465286bf52a0878bb
Stephan Lamoureux <sa.lamoureux@gmail.com>
2021-11-20 09:31:00 -0500

    fixed article links from having the same id

--- scripts/blog.js
+++ scripts/blog.js
@@ -8,10 +8,10 @@ async function getArticle() {
   // const profilePic = data[0].user.profile_image
   const tag = data[0].tag_list
   const name = data[0].user.username
-  const cover = data[0].cover_image
+  // const cover = data[0].cover_image
 
   document.getElementById('article_title').textContent = title
-  document.getElementById('devArticle').href = link
+  document.getElementById('article_link').href = link
   // document.getElementById('profile_image').src = profilePic
   document.getElementById('tags').textContent = tag.map(i => '#' + i)
   document.getElementById('username').textContent = '@' + name
@@ -23,7 +23,7 @@ async function getArticle() {
       element.className = 'devArticle'
 
       let articleLink = document.createElement('a') //link
-      articleLink.id = 'devArticle'
+      articleLink.id = 'article_link' + i
       articleLink.target = '_blank'
       articleLink.href = data[i].url
 

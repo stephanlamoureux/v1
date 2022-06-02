a23d985e80a27ea8953e1703e928363a47d2637a
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-06-02 16:50:01 -0400

    updated about me page

--- scripts/main.js
+++ scripts/main.js
@@ -22,8 +22,8 @@ var typed = new Typed('#typed', {
   strings: [
     'A Frontend Fanatic.',
     "^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
-    '^2000 An Over-Caffeinated Coder.',
     '^2000 The Duke of Dark Mode.',
+    '^2000 An Over-Caffeinated Coder.',
   ],
   typeSpeed: 70,
   loop: true,

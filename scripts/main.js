9d602c5e2cae515fa87fcbf29e2f295e8b48592f
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-06-14 17:52:49 -0400

    added more styling

--- scripts/main.js
+++ scripts/main.js
@@ -51,7 +51,7 @@ links.forEach(link => {
 
 function lightMode() {
   document.querySelectorAll('about-p').forEach(p => p.classList.toggle('light-mode'))
-  document.body.classList.toggle('light-background')
+  document.body.classList.toggle('light-mode')
   document.querySelector('.fa-sun').classList.toggle('fa-moon')
   document.querySelector('.paragraph .about-header').classList.toggle('light-mode')
   document.querySelector('.paragraph .working-on-header').classList.toggle('light-mode')

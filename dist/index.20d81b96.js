dd0b668e20e6998cc51095de980ed65c8bcf318d
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-12-03 16:40:57 -0500

    fixed css transition

--- dist/index.20d81b96.js
+++ dist/index.20d81b96.js
@@ -538,7 +538,7 @@ var _typedJsDefault = parcelHelpers.interopDefault(_typedJs);
 const typed = new (0, _typedJsDefault.default)("#typed", {
     strings: [
         "",
-        '^2000cat welcome.txt ^1400 <br> `Hey, I\'m Steve.<br>I love to build (and break) things on the web.``<br> <span class="prompt">\u2192 <span class="cyan">v1</span> <span class="purple">git:</span><span class="purple">(</span><span class="red">main</span><span class="purple">)</span></span>&nbsp;`', 
+        '^2000cat welcome.txt ^1400 <br> `Hey, I\'m Steve.<br>I love to build (and break) things on the web.``<br> <span class="prompt">\u2192 <span class="cyan-term">v1</span> <span class="purple-term">git:</span><span class="purple-term">(</span><span class="red-term">main</span><span class="purple-term">)</span></span>&nbsp;`', 
     ],
     typeSpeed: 70,
     loop: false,
--- scripts/typed.js
+++ scripts/typed.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js'
 const typed = new Typed('#typed', {
 	strings: [
 		'',
-		'^2000cat welcome.txt ^1400 <br> `Hey, I\'m Steve.<br>I love to build (and break) things on the web.``<br> <span class="prompt">→ <span class="cyan">v1</span> <span class="purple">git:</span><span class="purple">(</span><span class="red">main</span><span class="purple">)</span></span>&nbsp;`',
+		'^2000cat welcome.txt ^1400 <br> `Hey, I\'m Steve.<br>I love to build (and break) things on the web.``<br> <span class="prompt">→ <span class="cyan-term">v1</span> <span class="purple-term">git:</span><span class="purple-term">(</span><span class="red-term">main</span><span class="purple-term">)</span></span>&nbsp;`',
 	],
 	typeSpeed: 70,
 	loop: false,

7331ff87ed7ce10a5df02f391bc745edd3f94aff
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-08-15 17:08:41 -0400

    refactored modal logic

--- dist/pages/projects.d890cb10.js
+++ dist/pages/projects.d890cb10.js
@@ -532,77 +532,42 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"4x9yv":[function(require,module,exports) {
-// TODO: Refactor modal logic
-// Project card modals
 const body = document.querySelector("body");
-const modal = document.getElementsByClassName("modal");
-const trigger = document.getElementsByClassName("trigger");
-const closeButton = document.getElementsByClassName("close-button");
-// When the user clicks the project image, open the modal
-trigger[0].onclick = function() {
-    modal[0].classList.toggle("show-modal");
-    if (modal[0].classList.contains("show-modal")) // Disable scroll
-    body.style.overflow = "hidden";
-    else // Enable scroll
+const modals = document.querySelectorAll(".modal");
+const triggers = document.querySelectorAll(".trigger");
+const closeButtons = document.querySelectorAll(".close-button");
+function toggleModal(index) {
+    modals[index].classList.toggle("show-modal");
+    if (modals[index].classList.contains("show-modal")) body.style.overflow = "hidden";
+    else body.style.overflow = "auto";
+}
+function closeModal(index) {
+    modals[index].classList.toggle("show-modal");
     body.style.overflow = "auto";
-};
-trigger[1].onclick = function() {
-    modal[1].classList.toggle("show-modal");
-    if (modal[1].classList.contains("show-modal")) // Disable scroll
-    body.style.overflow = "hidden";
-    else // Enable scroll
+}
+function closeAllModals() {
+    modals.forEach((modal)=>{
+        modal.classList.remove("show-modal");
+    });
     body.style.overflow = "auto";
+}
+triggers.forEach((trigger, index)=>{
+    trigger.onclick = ()=>toggleModal(index);
+});
+closeButtons.forEach((closeButton, index)=>{
+    closeButton.onclick = ()=>closeModal(index);
+});
+window.onclick = (event)=>{
+    modals.forEach((modal, index)=>{
+        if (event.target === modal) closeModal(index);
+    });
 };
-trigger[2].onclick = function() {
-    modal[2].classList.toggle("show-modal");
-    if (modal[2].classList.contains("show-modal")) // Disable scroll
-    body.style.overflow = "hidden";
-    else // Enable scroll
-    body.style.overflow = "auto";
-};
-trigger[3].onclick = function() {
-    modal[3].classList.toggle("show-modal");
-    if (modal[3].classList.contains("show-modal")) // Disable scroll
-    body.style.overflow = "hidden";
-    else // Enable scroll
-    body.style.overflow = "auto";
-};
-// When the user clicks on (x), close the modal
-closeButton[0].onclick = function() {
-    modal[0].classList.toggle("show-modal");
-    body.style.overflow = "auto";
-};
-closeButton[1].onclick = function() {
-    modal[1].classList.toggle("show-modal");
-    body.style.overflow = "auto";
-};
-closeButton[2].onclick = function() {
-    modal[2].classList.toggle("show-modal");
-    body.style.overflow = "auto";
-};
-closeButton[3].onclick = function() {
-    modal[3].classList.toggle("show-modal");
-    body.style.overflow = "auto";
-};
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal[0]) {
-        modal[0].classList.toggle("show-modal");
-        body.style.overflow = "auto";
-    }
-    if (event.target == modal[1]) {
-        modal[1].classList.toggle("show-modal");
-        body.style.overflow = "auto";
-    }
-    if (event.target == modal[2]) {
-        modal[2].classList.toggle("show-modal");
-        body.style.overflow = "auto";
-    }
-    if (event.target == modal[3]) {
-        modal[3].classList.toggle("show-modal");
-        body.style.overflow = "auto";
-    }
-};
+// Prevent scroll on modal open and close
+modals.forEach((modal)=>{
+    modal.addEventListener("scroll", (event)=>{
+        event.stopPropagation();
+    });
+});
 
 },{}]},["jRPG8","4x9yv"], "4x9yv", "parcelRequire390d")
 
--- scripts/projects.js
+++ scripts/projects.js
@@ -1,97 +1,48 @@
-// TODO: Refactor modal logic
+const body = document.querySelector('body');
+const modals = document.querySelectorAll('.modal');
+const triggers = document.querySelectorAll('.trigger');
+const closeButtons = document.querySelectorAll('.close-button');
 
-// Project card modals
-
-const body = document.querySelector('body')
-const modal = document.getElementsByClassName('modal')
-const trigger = document.getElementsByClassName('trigger')
-const closeButton = document.getElementsByClassName('close-button')
-
-// When the user clicks the project image, open the modal
-
-trigger[0].onclick = function () {
-	modal[0].classList.toggle('show-modal')
-	if (modal[0].classList.contains('show-modal')) {
-		// Disable scroll
-		body.style.overflow = 'hidden'
-	} else {
-		// Enable scroll
-		body.style.overflow = 'auto'
-	}
+function toggleModal(index) {
+  modals[index].classList.toggle('show-modal');
+  if (modals[index].classList.contains('show-modal')) {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = 'auto';
+  }
 }
 
-trigger[1].onclick = function () {
-	modal[1].classList.toggle('show-modal')
-	if (modal[1].classList.contains('show-modal')) {
-		// Disable scroll
-		body.style.overflow = 'hidden'
-	} else {
-		// Enable scroll
-		body.style.overflow = 'auto'
-	}
+function closeModal(index) {
+  modals[index].classList.toggle('show-modal');
+  body.style.overflow = 'auto';
 }
 
-trigger[2].onclick = function () {
-	modal[2].classList.toggle('show-modal')
-	if (modal[2].classList.contains('show-modal')) {
-		// Disable scroll
-		body.style.overflow = 'hidden'
-	} else {
-		// Enable scroll
-		body.style.overflow = 'auto'
-	}
+function closeAllModals() {
+  modals.forEach((modal) => {
+    modal.classList.remove('show-modal');
+  });
+  body.style.overflow = 'auto';
 }
 
-trigger[3].onclick = function () {
-	modal[3].classList.toggle('show-modal')
-	if (modal[3].classList.contains('show-modal')) {
-		// Disable scroll
-		body.style.overflow = 'hidden'
-	} else {
-		// Enable scroll
-		body.style.overflow = 'auto'
-	}
-}
+triggers.forEach((trigger, index) => {
+  trigger.onclick = () => toggleModal(index);
+});
 
-// When the user clicks on (x), close the modal
+closeButtons.forEach((closeButton, index) => {
+  closeButton.onclick = () => closeModal(index);
+});
 
-closeButton[0].onclick = function () {
-	modal[0].classList.toggle('show-modal')
-	body.style.overflow = 'auto'
-}
+window.onclick = (event) => {
+  modals.forEach((modal, index) => {
+    if (event.target === modal) {
+      closeModal(index);
+    }
+  });
+};
 
-closeButton[1].onclick = function () {
-	modal[1].classList.toggle('show-modal')
-	body.style.overflow = 'auto'
-}
-
-closeButton[2].onclick = function () {
-	modal[2].classList.toggle('show-modal')
-	body.style.overflow = 'auto'
-}
-
-closeButton[3].onclick = function () {
-	modal[3].classList.toggle('show-modal')
-	body.style.overflow = 'auto'
-}
-
-// When the user clicks anywhere outside of the modal, close it
-
-window.onclick = function (event) {
-	if (event.target == modal[0]) {
-		modal[0].classList.toggle('show-modal')
-		body.style.overflow = 'auto'
-	}
-	if (event.target == modal[1]) {
-		modal[1].classList.toggle('show-modal')
-		body.style.overflow = 'auto'
-	}
-	if (event.target == modal[2]) {
-		modal[2].classList.toggle('show-modal')
-		body.style.overflow = 'auto'
-	}
-	if (event.target == modal[3]) {
-		modal[3].classList.toggle('show-modal')
-		body.style.overflow = 'auto'
-	}
-}
+// Prevent scroll on modal open and close
+modals.forEach((modal) => {
+  modal.addEventListener('scroll', (event) => {
+    event.stopPropagation();
+  });
+});

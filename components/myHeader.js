53f119139c2ca27ddfa6d38273226c0ab46786f0
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-11-17 22:22:13 -0500

    please work :\

--- components/myHeader.js
+++ components/myHeader.js
@@ -2,6 +2,11 @@ import Typed from 'typed.js'
 
 class myHeader extends HTMLElement {
 	connectedCallback() {
+		this.render()
+		this.initializeTyped()
+	}
+
+	render() {
 		this.innerHTML = `
             <div class="center-header">
                 <h1 class="header-title">
@@ -16,39 +21,28 @@ class myHeader extends HTMLElement {
                 </p>
             </div>
         `
+	}
 
-		// Create an observer to monitor changes in the element
-		const observer = new MutationObserver(mutations => {
-			for (const mutation of mutations) {
-				// Check if the mutation added nodes
-				if (mutation.addedNodes.length) {
-					const typedElement = this.querySelector('#typed2')
-					if (typedElement) {
-						// Initialize Typed.js on the element
-						new Typed('#typed2', {
-							strings: [
-								'A Frontend Fanatic.',
-								"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
-								'^2000 A Caffeine Connoisseur.',
-								'^2000 The Duke of Dark Mode.',
-							],
-							typeSpeed: 70,
-							loop: true,
-							startDelay: 1000,
-							backDelay: 3000,
-							backSpeed: 50,
-						})
-
-						// Stop observing after initializing Typed.js
-						observer.disconnect()
-						break
-					}
-				}
-			}
-		})
-
-		// Start observing the element for changes
-		observer.observe(this, { childList: true, subtree: true })
+	initializeTyped() {
+		// Ensuring the element is in the DOM
+		if (this.querySelector('#typed2')) {
+			new Typed('#typed2', {
+				strings: [
+					'A Frontend Fanatic.',
+					"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
+					'^2000 A Caffeine Connoisseur.',
+					'^2000 The Duke of Dark Mode.',
+				],
+				typeSpeed: 70,
+				loop: true,
+				startDelay: 1000,
+				backDelay: 3000,
+				backSpeed: 50,
+			})
+		} else {
+			// Retry initialization after a brief delay
+			setTimeout(() => this.initializeTyped(), 100)
+		}
 	}
 }
 

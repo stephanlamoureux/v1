068430967ad6f89bf855d9c8e752b2c2314f11be
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-11-17 22:30:25 -0500

    fixed typed.js bug

--- components/myHeader.js
+++ components/myHeader.js
@@ -1,47 +1,19 @@
-import Typed from 'typed.js'
-
 class myHeader extends HTMLElement {
 	connectedCallback() {
-		this.render()
-		this.initializeTypedWithAnimationFrame()
-	}
-
-	render() {
 		this.innerHTML = `
-            <div class="center-header">
-                <h1 class="header-title">
-                    <span class="header-gradient">
-                        <span class="spacing">Stephan <span class="hidden">Lamoureu</span></span>
-                        <span class="hidden">x</span>
-                    </span>
-                    <span class="show header-gradient">L.</span>
-                </h1>
-                <p class="header-sub">
-                    <span id="typed2" class="typed2"></span>
-                </p>
-            </div>
-        `
-	}
-
-	initializeTypedWithAnimationFrame() {
-		requestAnimationFrame(() => {
-			requestAnimationFrame(() => {
-				// Double requestAnimationFrame ensures the DOM elements are truly rendered
-				new Typed('#typed2', {
-					strings: [
-						'A Frontend Fanatic.',
-						"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
-						'^2000 A Caffeine Connoisseur.',
-						'^2000 The Duke of Dark Mode.',
-					],
-					typeSpeed: 70,
-					loop: true,
-					startDelay: 1000,
-					backDelay: 3000,
-					backSpeed: 50,
-				})
-			})
-		})
+			<div class="center-header">
+				<h1 class="header-title">
+					<span class="header-gradient">
+						<span class="spacing">Stephan <span class="hidden">Lamoureu</span></span
+						><span class="hidden">x</span></span
+					>
+					<span class="show header-gradient">L.</span>
+				</h1>
+				<p class="header-sub">
+					<span id="typed2" class="typed2"></span>
+				</p>
+			</div>
+`
 	}
 }
 

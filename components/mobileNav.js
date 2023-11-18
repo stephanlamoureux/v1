32558e8d7c5ea624aceba205dbc18de03ae9910e
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-11-17 21:18:04 -0500

    added web components

---
+++ components/mobileNav.js
@@ -0,0 +1,64 @@
+class mobileNav extends HTMLElement {
+	connectedCallback() {
+		this.innerHTML = `
+		<!-- Mobile Menu -->
+		<div id="nav-wrapper" class="nav-wrapper about-nav-wrapper">
+			<button
+				class="menu hamburger hamburger--vortex"
+				type="button"
+				aria-label="Menu"
+				aria-controls="navigation"
+			>
+				<span class="hamburger-box">
+					<span class="hamburger-inner"></span>
+				</span>
+			</button>
+			<nav id="navigation" class="navbar">
+				<ul class="nav-list" role="menu">
+					<li class="menu-link" role="none">
+						<a href="index.html" role="menuitem">
+							<div
+								class="mobile-profile"
+								role="img"
+								aria-label="portfolio picture of Stephan"
+							></div>
+						</a>
+					</li>
+					<li class="menu-link" role="none">
+						<a href="index.html" role="menuitem">About</a>
+					</li>
+					<li class="menu-link" role="none">
+						<a href="pages/blog.html" role="menuitem">Blog</a>
+					</li>
+					<li class="menu-link" role="none">
+						<a href="pages/projects.html" role="menuitem">Projects</a>
+					</li>
+					<li class="menu-link" role="none">
+						<a href="pages/contact.html" role="menuitem">Contact</a>
+					</li>
+					<!-- Mobile Color Mode Toggle -->
+					<li role="none">
+						<div class="mode-toggle" role="menuitem">
+							<input
+								type="checkbox"
+								class="mobile-mode-toggle checkbox"
+								id="mobile-checkbox"
+								aria-label="color mode selector"
+							/>
+							<label for="mobile-checkbox" class="checkbox-label">
+								<i class="fas fa-moon" alt="moon icon"></i>
+								<i class="fas fa-sun" alt="sun icon"></i>
+								<div class="ball"></div>
+							</label>
+						</div>
+					</li>
+					<!-- End Mobile Color Mode Toggle -->
+				</ul>
+			</nav>
+		</div>
+		<!-- End Mobile Menu -->
+`
+	}
+}
+
+customElements.define('mobile-nav', mobileNav)
---
+++ dist/index.6dd637a6.js
@@ -0,0 +1,49 @@
+class desktopNav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+		<!-- Desktop Nav -->
+		<div class="nav-desktop">
+			<ul class="desktop-list" role="menu">
+				<li role="none">
+					<a href="index.html" role="menuitem">
+						<div class="brand" role="img" aria-label="portfolio picture of Stephan"></div>
+					</a>
+				</li>
+				<li class="desktop-link" role="none">
+					<a class="link is-active" href="index.html" role="menuitem">About</a>
+				</li>
+				<li class="desktop-link" role="none">
+					<a class="link" href="pages/blog.html" role="menuitem">Blog</a>
+				</li>
+				<li class="desktop-link" role="none">
+					<a class="link" href="pages/projects.html" role="menuitem">Projects</a>
+				</li>
+				<li class="desktop-link" role="none">
+					<a class="link" href="pages/contact.html" role="menuitem">Contact</a>
+				</li>
+				<!-- Desktop Color Mode Toggle -->
+				<li role="none">
+					<div class="mode-toggle" role="menuitem">
+						<input
+							type="checkbox"
+							class="checkbox"
+							id="desktop-checkbox"
+							aria-label="color mode selector"
+						/>
+						<label for="desktop-checkbox" class="checkbox-label">
+							<i class="fas fa-moon" alt="moon icon"></i>
+							<i class="fas fa-sun" alt="sun icon"></i>
+							<div class="ball"></div>
+						</label>
+					</div>
+				</li>
+				<!-- End Desktop Color Mode Toggle -->
+			</ul>
+		</div>
+		<!-- End Desktop Nav -->
+`;
+    }
+}
+customElements.define("desktop-nav", desktopNav);
+
+//# sourceMappingURL=index.6dd637a6.js.map

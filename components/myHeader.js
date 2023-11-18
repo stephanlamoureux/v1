79fde917536580e820d4eadfe22425d45a181d5f
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-11-17 22:26:49 -0500

    ...

--- components/myHeader.js
+++ components/myHeader.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js'
 class myHeader extends HTMLElement {
 	connectedCallback() {
 		this.render()
-		this.initializeTyped()
+		this.initializeTypedWithAnimationFrame()
 	}
 
 	render() {
@@ -23,26 +23,25 @@ class myHeader extends HTMLElement {
         `
 	}
 
-	initializeTyped() {
-		// Ensuring the element is in the DOM
-		if (this.querySelector('#typed2')) {
-			new Typed('#typed2', {
-				strings: [
-					'A Frontend Fanatic.',
-					"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
-					'^2000 A Caffeine Connoisseur.',
-					'^2000 The Duke of Dark Mode.',
-				],
-				typeSpeed: 70,
-				loop: true,
-				startDelay: 1000,
-				backDelay: 3000,
-				backSpeed: 50,
+	initializeTypedWithAnimationFrame() {
+		requestAnimationFrame(() => {
+			requestAnimationFrame(() => {
+				// Double requestAnimationFrame ensures the DOM elements are truly rendered
+				new Typed('#typed2', {
+					strings: [
+						'A Frontend Fanatic.',
+						"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
+						'^2000 A Caffeine Connoisseur.',
+						'^2000 The Duke of Dark Mode.',
+					],
+					typeSpeed: 70,
+					loop: true,
+					startDelay: 1000,
+					backDelay: 3000,
+					backSpeed: 50,
+				})
 			})
-		} else {
-			// Retry initialization after a brief delay
-			setTimeout(() => this.initializeTyped(), 100)
-		}
+		})
 	}
 }
 

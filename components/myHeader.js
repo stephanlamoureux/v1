2b02f6818aa4b32a6d915edc9130cee0c8d55174
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-11-17 21:56:35 -0500

    attepmpt 1 of fixing typed.js bug

--- components/myHeader.js
+++ components/myHeader.js
@@ -16,20 +16,21 @@ class myHeader extends HTMLElement {
 								</p>
 						</div>
 				`
-
-		new Typed('#typed2', {
-			strings: [
-				'A Frontend Fanatic.',
-				"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
-				'^2000 A Caffeine Connoisseur.',
-				'^2000 The Duke of Dark Mode.',
-			],
-			typeSpeed: 70,
-			loop: true,
-			startDelay: 1000,
-			backDelay: 3000,
-			backSpeed: 50,
-		})
+		if (document.querySelector('#typed2')) {
+			new Typed('#typed2', {
+				strings: [
+					'A Frontend Fanatic.',
+					"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
+					'^2000 A Caffeine Connoisseur.',
+					'^2000 The Duke of Dark Mode.',
+				],
+				typeSpeed: 70,
+				loop: true,
+				startDelay: 1000,
+				backDelay: 3000,
+				backSpeed: 50,
+			})
+		}
 	}
 }
 

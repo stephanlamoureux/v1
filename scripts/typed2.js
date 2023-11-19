77645d14485f7e6136edb7a83c0bcc1a19befd5f
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-11-19 15:06:52 -0500

    changed the way typed.js is working

--- scripts/typed2.js
+++ scripts/typed2.js
@@ -1,15 +1,27 @@
 import Typed from 'typed.js'
 
-const typed2 = new Typed('#typed2', {
-	strings: [
-		'A Frontend Fanatic.',
-		"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
-		'^2000 A Caffeine Connoisseur.',
-		'^2000 The Duke of Dark Mode.',
-	],
-	typeSpeed: 70,
-	loop: true,
-	startDelay: 1000,
-	backDelay: 3000,
-	backSpeed: 50,
+function initializeTyped2() {
+	const element = document.querySelector('#typed2')
+	if (element) {
+		new Typed('#typed2', {
+			strings: [
+				'A Frontend Fanatic.',
+				"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
+				'^2000 A Caffeine Connoisseur.',
+				'^2000 The Duke of Dark Mode.',
+			],
+			typeSpeed: 70,
+			loop: true,
+			startDelay: 1000,
+			backDelay: 3000,
+			backSpeed: 50,
+		})
+	} else {
+		// Retry initialization after a brief delay
+		setTimeout(initializeTyped2, 100)
+	}
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	initializeTyped2()
 })

37a0dc6c07af060434cb44cc756ba154fb6b0dba
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-11-19 15:41:00 -0500

    attempt to fix typed.js bug

--- scripts/typed2.js
+++ scripts/typed2.js
@@ -1,24 +1,28 @@
+import Typed from 'typed.js'
+
 function initializeTyped2() {
-	if (document.readyState === 'loading') {
-		document.addEventListener('DOMContentLoaded', initializeTyped2)
+	// Check if the element exists
+	const element = document.querySelector('#typed2')
+	if (element) {
+		// Initialize Typed.js on the element
+		new Typed('#typed2', {
+			strings: [
+				'A Frontend Fanatic.',
+				"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
+				'^2000 A Caffeine Connoisseur.',
+				'^2000 The Duke of Dark Mode.',
+			],
+			typeSpeed: 70,
+			loop: true,
+			startDelay: 1000,
+			backDelay: 3000,
+			backSpeed: 50,
+		})
 	} else {
-		const element = document.querySelector('#typed2')
-		if (element) {
-			new Typed('#typed2', {
-				strings: [
-					'A Frontend Fanatic.',
-					"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
-					'^2000 A Caffeine Connoisseur.',
-					'^2000 The Duke of Dark Mode.',
-				],
-				typeSpeed: 70,
-				loop: true,
-				startDelay: 1000,
-				backDelay: 3000,
-				backSpeed: 50,
-			})
-		}
+		// If the element is not found, retry after a brief delay
+		setTimeout(initializeTyped2, 100)
 	}
 }
 
+// Immediately call the function
 initializeTyped2()

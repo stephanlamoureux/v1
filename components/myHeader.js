f2dbc033c73dc4301b65d83dcc08549778d7c0a7
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-11-19 11:29:14 -0500

    changing order of component imports

--- components/myHeader.js
+++ components/myHeader.js
@@ -3,23 +3,20 @@ import Typed from 'typed.js'
 class myHeader extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = `
-				<div class="center-header">
-				<h1 class="header-title">
-					<span class="header-gradient">
-						<span class="spacing">Stephan <span class="hidden">Lamoureu</span></span
-						><span class="hidden">x</span></span
-					>
-					<span class="show header-gradient">L.</span>
-				</h1>
-				<p class="header-sub">
-					<span id="typed2" class="typed2"></span>
-				</p>
-			</div>
-        `
-	}
+						<div class="center-header">
+								<h1 class="header-title">
+										<span class="header-gradient">
+												<span class="spacing">Stephan <span class="hidden">Lamoureu</span></span
+												><span class="hidden">x</span></span
+										>
+										<span class="show header-gradient">L.</span>
+								</h1>
+								<p class="header-sub">
+										<span id="typed2" class="typed2"></span>
+								</p>
+						</div>
+				`
 
-	// Public method to initialize Typed.js
-	initializeTyped() {
 		new Typed('#typed2', {
 			strings: [
 				'A Frontend Fanatic.',
